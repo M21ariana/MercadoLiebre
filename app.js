75d9963d5141b7c0de8ad82f1dc90ab2d154d191
M21ariana <m.montoya2107@gmail.com>
2022-12-20 22:29:48 -0500

    subida

---
+++ app.js
@@ -0,0 +1,17 @@
+
+const express = require ('express');
+const path = require ('path');
+
+const app = express();
+
+const publicPath = path.resolve(__dirname,'./public'); //Decirle a express cuál va a ser nuestra carpeta de archivos pública (archivos estáticos)
+app.use(express.static(publicPath));
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo");
+  })
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/home.html'));
+})
+
